1a3e563be8f1bb7ccb4276f910fdf4066c0df679
Carolyne Golias <goliascarolyne@gmail.com>
2024-12-16 22:31:30 +0000

    worked on function is vowel

--- string-manipulation-1/src/is-vowel.ts
+++ string-manipulation-1/src/is-vowel.ts
@@ -1,1 +1,19 @@
 /* exported isVowel */
+// function isVowel(char: string): boolean {
+
+//   for (let i = 0; i < char.length; i++) {
+//     if (char[i] === 'a' || 'A') {
+//       return true;
+//     } else if (char[i] === 'e' || 'E') {
+//       return true;
+//     } else if (char[i] === 'i' || 'I') {
+//       return true;
+//     } else if (char[i] === 'o' || 'O') {
+//       return true;
+//     } else if (char[i] === 'u' || 'U') {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+// }
