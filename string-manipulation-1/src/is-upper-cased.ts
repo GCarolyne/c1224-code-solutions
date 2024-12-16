fdb14c320616161045f9fcdc20aa0ea5c9efb33d
Carolyne Golias <goliascarolyne@gmail.com>
2024-12-16 17:09:51 +0000

    task is upper cased

--- string-manipulation-1/src/is-upper-cased.ts
+++ string-manipulation-1/src/is-upper-cased.ts
@@ -1,1 +1,11 @@
 /* exported isUpperCased */
+// function isUpperCased(word: string): boolean {
+//   const reWord = '';
+//   for (let i = 0; i < word.length; i++) {
+//     if (word[i] === word.toUpperCase()) {
+//       reWord += word.toUpperCase();
+//     } else {
+//       return false;
+//     }
+//   }
+// }
