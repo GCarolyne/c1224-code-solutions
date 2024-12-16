1747c79ff5b78671f0b7388cab1869c1291d4634
Carolyne Golias <goliascarolyne@gmail.com>
2024-12-16 19:11:04 +0000

    updated islowercase function

--- string-manipulation-1/src/is-upper-cased.ts
+++ string-manipulation-1/src/is-upper-cased.ts
@@ -1,11 +1,9 @@
 /* exported isUpperCased */
-// function isUpperCased(word: string): boolean {
-//   const reWord = '';
-//   for (let i = 0; i < word.length; i++) {
-//     if (word[i] === word.toUpperCase()) {
-//       reWord += word.toUpperCase();
-//     } else {
-//       return false;
-//     }
-//   }
-// }
+function isUpperCased(word: string): boolean {
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] !== word[i].toUpperCase()) {
+      return false;
+    }
+  }
+  return true;
+}
