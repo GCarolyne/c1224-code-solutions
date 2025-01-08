c2afb8aef57794df56654f4c0c24adac0b1022e2
Carolyne Golias <goliascarolyne@gmail.com>
2025-01-08 18:21:50 +0000

    deleted console logs

--- dom-event-delegation/main.ts
+++ dom-event-delegation/main.ts
@@ -3,11 +3,8 @@ if (!$taskList) throw new Error('query task list ul failed.');
 
 $taskList.addEventListener('click', (event: Event) => {
   const eventTarget = event.target as HTMLElement;
-  console.log('eventTarget', eventTarget);
-  console.log('eventTarget.tagName', eventTarget.tagName);
   if (eventTarget.tagName === 'BUTTON') {
     const taskElement = eventTarget.closest('.task-list-item') as HTMLElement;
-    console.log('taskElement', taskElement);
     taskElement.remove();
   }
 });
