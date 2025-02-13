4135b5d5ac666bd6654dec23d726919725ffa6b2
Carolyne Golias <goliascarolyne@gmail.com>
2025-02-13 04:54:05 +0000

    react list to be continued

---
+++ react-lists/my-app/src/App.tsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react';
+// import { PokemonList } from './PokemonList';
+// import './App.css';
+
+// export type Pokemon = {
+//   number: string;
+//   name: string;
+// };
+
+// const pokedex: Pokemon[] = [
+//   { number: '001', name: 'Bulbasaur' },
+//   { number: '004', name: 'Charmander' },
+//   { number: '007', name: 'Squirtle' },
+//   { number: '025', name: 'Pikachu' },
+//   { number: '039', name: 'Jigglypuff' },
+// ];
+
+// export default function App() {
+//   return (
+//     <>
+//       <PokemonList nameValue={pokedex} />
+//     </>
+//   );
+// }
---
+++ react-lists/my-app/src/PokemonList.tsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react';
+// import { Pokemon } from './App';
+
+// type Props = {
+//   pokedex: Pokemon[];
+// };
+
+// export function PokemonList({ pokedex }: Props) {
+//   return (
+//     <>
+//       <ul>
+//         <li>{pokedex}</li>
+//         <li>{pokedex}</li>
+//         <li>{pokedex}</li>
+//         <li>{pokedex}</li>
+//         <li>{pokedex}</li>
+
+//       </ul>
+//     </>
+//   );
+// }
