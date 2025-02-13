8e70821dc0550daeda41b1d2a4fa63c83b868449
Carolyne Golias <goliascarolyne@gmail.com>
2025-02-13 19:11:20 +0000

    added key attribute to li child with pokemon.number value

--- react-lists/my-app/src/PokemonList.tsx
+++ react-lists/my-app/src/PokemonList.tsx
@@ -5,10 +5,8 @@ type Props = {
 };
 
 export function PokemonList({ poxedex }: Props) {
-  const listPoke = poxedex.map((pokemon) => <li>{pokemon.name}</li>);
-  return (
-    <>
-      <ul>{listPoke}</ul>
-    </>
-  );
+  const listPoke = poxedex.map((pokemon) => (
+    <li key={pokemon.number}>{pokemon.name}</li>
+  ));
+  return <ul>{listPoke}</ul>;
 }
