0231d67dffaa26f22f44c23ee7dc8df6c6e83d37
Carolyne Golias <goliascarolyne@gmail.com>
2025-02-18 19:14:36 +0000

    completed react effects

---
+++ react-effects/my-app/src/List.tsx
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unused-vars -- Remove me */
+import { useEffect, useState } from 'react';
+import { readItems, type Item } from './read';
+
+export function List() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [items, setItems] = useState<Item[]>([]);
+  const [error, setError] = useState<unknown>();
+
+  useEffect(() => {
+    async function work() {
+      try {
+        const read = await readItems();
+        setItems(read);
+      } catch (error) {
+        setError(error);
+      }
+      setIsLoading(false);
+    }
+    work();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return (
+      <div>
+        Error! {error instanceof Error ? error.message : 'Unknown Error'}
+      </div>
+    );
+  }
+
+  return (
+    <ul>
+      {items.map((item) => (
+        <li key={item.id}>
+          {item.id}: {item.name}
+        </li>
+      ))}
+    </ul>
+  );
+}
