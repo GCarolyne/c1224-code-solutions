a4efddeb26cd0a43365b3c12709f7349f42fe190
Carolyne Golias <goliascarolyne@gmail.com>
2025-02-23 20:42:01 +0000

    made suggested changes

--- react-effects/my-app/src/List.tsx
+++ react-effects/my-app/src/List.tsx
@@ -14,8 +14,9 @@ export function List() {
         setItems(read);
       } catch (error) {
         setError(error);
+      } finally {
+        setIsLoading(false);
       }
-      setIsLoading(false);
     }
     work();
   }, []);
