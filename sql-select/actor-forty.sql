57112cec6c9918333e96920996932bb00f532fbb
Carolyne Golias <goliascarolyne@gmail.com>
2025-02-27 22:39:54 +0000

    sql-select

---
+++ sql-select/actor-forty.sql
@@ -0,0 +1,5 @@
+SELECT "actorId","firstName","lastName" FROM "actors" WHERE "actorId" = '40';
+ actorId | firstName | lastName
+---------+-----------+----------
+      40 | Johnny    | Cage
+(1 row)
