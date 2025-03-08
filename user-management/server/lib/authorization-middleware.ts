63bc76d3c9792229337a2049e8d6a5975413a458
Carolyne Golias <goliascarolyne@gmail.com>
2025-03-08 00:49:23 +0000

    update

--- user-management/server/lib/authorization-middleware.ts
+++ user-management/server/lib/authorization-middleware.ts
@@ -12,11 +12,14 @@ export function authMiddleware(
   res: Response,
   next: NextFunction
 ): void {
-  const header = req.get('Authorization');
-  const parsedToken = req.get('Authorization')?.split('Bearer ')[1];
+  // the token will be in the authorization header with the format Bearer token
+  const header = req.get('Authorization'); //* this will return the value of a header it will return value or undefined
+  const parsedToken = req.get('Authorization')?.split('Bearer ')[1]; //* this splits the array, Bearer , aad second element of the array [1] would be a token.
   if (!header || !parsedToken)
     throw new ClientError(401, 'authentication required');
-  const verifyToken = jwt.verify(parsedToken, hashKey);
+  const verifyToken = jwt.verify(parsedToken, hashKey); // this is where the payload is returned and this compares the paresedToken and haskey
   req.user = verifyToken as Request['user'];
   next();
 }
+
+//* The request.user will have the payload information about the clientInformation.Line 20 the request is sent from a user to user.key.
